9e138caa0e58e235df72541ac236c96b23df4659
llevieux <llevieux@lucaslevieux.com>
2015-12-04 22:05:37 -0800

    Added Kane

--- src/futility/Room.java
+++ src/futility/Room.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Lucas LeVieux <contact@lucaslevieux.com>
+ * Copyright (C) 2015 Lucas LeVieux & Kane McGrath
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
