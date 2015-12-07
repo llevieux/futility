6e6689bf234d2e728f5fbbc039b0578c1eadf890
llevieux <llevieux@users.noreply.github.com>
2015-12-07 11:51:35 -0800

    run in full screen + remarks

--- src/runme.bat
+++ src/runme.bat
@@ -1,2 +1,8 @@
-@title futility: the game
-@java -jar Futility.jar
+@echo off
+rem a short batch file to run the jar file (if you open it directly, it doesn't do command line stuff for some reason)
+
+rem names the window
+title futility: the game
+
+rem starts in fullscreen
+start /max java -jar Futility.jar
