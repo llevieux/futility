42b686e9002f37d34074c769fc62cf99b6f01970
llevieux <llevieux@lucaslevieux.com>
2015-12-07 16:46:45 -0800

    basically reverting (fullscreen isn't possible, and probably not desirable)

--- src/runme.bat
+++ src/runme.bat
@@ -1,8 +1,7 @@
 @echo off
-rem a short batch file to run the jar file (if you open it directly, it doesn't do command line stuff for some reason)
 
-rem names the window
+rem A short batch file to run the jar file (if you open it directly, it doesn't 
+rem do command line stuff for some reason)
+
 title futility: the game
-
-rem starts in fullscreen
-start /max java -jar Futility.jar
+java -jar Futility.jar
