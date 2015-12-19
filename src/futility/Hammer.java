08dd107e5184c039147b034927815b7760386f83
Kane <Kane@192.168.33.102>
2015-12-18 17:14:54 -0800

    Created Hammer Class and set the vars

---
+++ src/futility/Hammer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package futility;
+
+/**
+ *
+ * @author Kane
+ */
+public class Hammer extends Item{
+    public Hammer(String name){
+        super(name);
+        isFlammable = false;
+        isMoveable = true;
+        
+    }
+}
