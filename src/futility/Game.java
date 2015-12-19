b90a65ac770293ec7e7407ea31bd9b8eb37ee3a6
Kane <Kane@192.168.33.102>
2015-12-18 22:16:09 -0800

    Added descriptions for base items

--- src/futility/Game.java
+++ src/futility/Game.java
@@ -140,7 +140,11 @@ public class Game
             buttons[i] = new UselessSwitch("button " + (i+1));
         
         Item monopolyMoney = new Item("monopoly money", true, true);
+        monopolyMoney.setDescription("The troubles of money are so foolish "
+                + "relative to the current situation");
         UselessSwitch pressureCooker = new UselessSwitch("pressure cooker");
+        pressureCooker.setDescription("The cruel Irony of having no food, and a "
+                + "Device to cook it with");
         //Chalk chalk = new Chalk("chalk");
         
         Item[] theRoomItems = {matchbox, flare, hammer, refrigerator, lawnMower,
