8c002f611e74f005bec5d82f68bfa3775f40ac2f
llevieux <llevieux@lucaslevieux.com>
2015-12-24 13:03:32 -0800

    reset cmd window to appropriate size and color

--- src/runme.bat
+++ src/runme.bat
@@ -3,5 +3,7 @@
 rem A short batch file to run the jar file (if you open it directly, it doesn't 
 rem do command line stuff for some reason)
 
+color 07
+mode con: cols=80 lines=25
 title futility: the game
 java -jar Futility.jar
