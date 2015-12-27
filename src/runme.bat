efde2836fbcbc76fc47ef5f33926e81253272c2f
llevieux <llevieux@lucaslevieux.com>
2015-12-26 18:04:07 -0800

    now checks for current version of java and Futility.jar

--- src/runme.bat
+++ src/runme.bat
@@ -1,9 +1,128 @@
 @echo off
 
-rem A short batch file to run the jar file (if you open it directly, it doesn't 
-rem do command line stuff for some reason)
+if exist Futility.jar (
+    goto CHECK_FOR_JAVA
+) else (
+    echo Futility.jar can't be found.  ensure that the directory is unzipped. 
+    echo.
+    echo if that doesn't fix things, try downloading the official build at 
+    echo        github.com/llevieux/futility/releases
+    echo.
+    pause
+    exit /B
+)
+
+
+:CHECK_FOR_JAVA
+rem check for java.  modified from: https://coderwall.com/p/ww4d9a/windows-search-for-java-installation-in-registry-using-batch-script
+
+setlocal ENABLEEXTENSIONS
+
+if %PROCESSOR_ARCHITECTURE%==AMD64 goto CHECK_JAVA_64
+if %PROCESSOR_ARCHITECTURE%==x86 goto CHECK_JAVA_32
+
+:CHECK_JAVA_64
+set KEY_NAME="HKEY_LOCAL_MACHINE\Software\Wow6432Node\JavaSoft\Java Runtime Environment"
+set VALUE_NAME=CurrentVersion
+set VALUE_FAMILYNAME=Java6FamilyVersion
+
+for /F "usebackq skip=2 tokens=1-3" %%A in (`REG QUERY %KEY_NAME% /v %VALUE_NAME% 2^>nul`) DO (
+    set ValueName=%%A
+    set ValueType=%%B
+    set ValueValue=%%C
+)
+@echo CurrentVersion %ValueValue%
+
+if "%ValueValue%" == "1.6" (
+    for /F "usebackq skip=2 tokens=1-3" %%A in (`REG QUERY %KEY_NAME% /v %VALUE_FAMILYNAME% 2^>nul`) DO (
+        set ValueFamilyName=%%A
+        set ValueFamilyType=%%B
+        set ValueFamilyValue=%%C
+    )
+)
+if "%ValueValue%" == "1.6" (
+    if "%ValueFamilyValue%" LSS "1.6.0_16" goto JAVA_NOT_INSTALLED
+)
+
+SET KEY_NAME="%KEY_NAME:~1,-1%\%ValueValue%"
+SET VALUE_NAME=JavaHome
+
+if defined ValueName (
+    for /F "usebackq skip=2 tokens=1,2*" %%A in (`REG QUERY %KEY_NAME% /v %VALUE_NAME% 2^>nul`) DO (
+        set ValueName2=%%A
+        set ValueType2=%%B
+        set JRE_PATH2=%%C
+
+        if defined ValueName2 (
+            set ValueName = %ValueName2%
+            set ValueType = %ValueType2%
+            set ValueValue =  %JRE_PATH2%
+        )
+    )
+)
+
+if NOT "%JRE_PATH2%" == "" goto START_FUTILITY
+else goto JAVA_NOT_INSTALLED
+
+:CHECK_JAVA_32
+set KEY_NAME="HKEY_LOCAL_MACHINE\Software\JavaSoft\Java Runtime Environment"
+set VALUE_NAME=CurrentVersion
+set VALUE_FAMILYNAME=Java6FamilyVersion
+
+for /F "usebackq skip=2 tokens=1-3" %%A in (`REG QUERY %KEY_NAME% /v %VALUE_NAME% 2^>nul`) DO (
+    set ValueName=%%A
+    set ValueType=%%B
+    set ValueValue=%%C
+)
+@echo CurrentVersion %ValueValue%
+if "%ValueValue%" == "" goto CHECK_JAVA_32
+
+if "%ValueValue%" == "1.6" (
+    for /F "usebackq skip=2 tokens=1-3" %%A in (`REG QUERY %KEY_NAME% /v %VALUE_FAMILYNAME% 2^>nul`) DO (
+        set ValueFamilyName=%%A
+        set ValueFamilyType=%%B
+        set ValueFamilyValue=%%C
+    )
+)
+if "%ValueValue%" == "1.6" (
+    if "%ValueFamilyValue%" LSS "1.6.0_16" goto JAVA_NOT_INSTALLED
+)
+
+SET KEY_NAME="%KEY_NAME:~1,-1%\%ValueValue%"
+SET VALUE_NAME=JavaHome
+
+if defined ValueName (
+    for /F "usebackq skip=2 tokens=1,2*" %%A in (`REG QUERY %KEY_NAME% /v %VALUE_NAME% 2^>nul`) DO (
+        set ValueName2=%%A
+        set ValueType2=%%B
+        set JRE_PATH2=%%C
+
+        if defined ValueName2 (
+            set ValueName = %ValueName2%
+            set ValueType = %ValueType2%
+            set ValueValue =  %JRE_PATH2%
+        )
+    )
+)
+
+if "%JRE_PATH2%" == ""  goto JAVA_NOT_INSTALLED
+else goto START_FUTILITY
+
+:JAVA_NOT_INSTALLED
+echo a current version of Java doesn't appear to be installed on your system :(
+echo.
+echo you'll need to download it in order for this to work.
+echo.
+echo.
+start "" http://www.java.com/en/download/
+pause
+exit /B
+
+:START_FUTILITY
+rem run the jar file (if you open it directly, it doesn't do command line stuff for some reason)
 
 color 07
 mode con: cols=80 lines=25
 title futility: the game
+cls
 java -jar Futility.jar
